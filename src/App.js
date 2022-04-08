7321d6842f2752dcf1cac640e3f5655d95bdd5b8
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-04-07 21:05:38 -0400

    add descriptions to allParams

--- src/App.js
+++ src/App.js
@@ -5,52 +5,52 @@ import { Sketch } from './Sketch';
 const allParams = {
   gridCount: {
     name: 'Grid Count',
-    description: '',
+    description: 'The number of rows and columns in the grid',
     value: 40,
   },
   filterPercent: {
     name: 'Filter Percent',
-    description: '',
+    description: 'The percent of symbols to randomly filter out (0-100)',
     value: 20,
   },
   colorCount: {
     name: 'Colour Count',
-    description: '',
+    description: 'The number of colours to use from the palette (0-5)',
     value: 5,
   },
   symbol: {
     name: 'Symbol',
-    description: '',
+    description: 'The Unicode symbol used in each cell. Try a |, =, or an emoji',
     value: '.',
   },
   rotationMultiplier: {
     name: 'Rotation Multiplier',
-    description: '',
+    description: 'Total rotation multiplier for each cell',
     value: 1,
   },
   radiusMultiplier: {
     name: 'Radius Multiplier',
-    description: '',
+    description: 'Total radius multiplier for each cell. Think of this as the size.',
     value: 0.25,
   },
   radiusXMultiplier: {
-    name: 'Radius X Multiplier',
-    description: '',
+    name: 'Radius Noise X Multiplier',
+    description: 'Multiplies against the X value in the noise generator for radius',
     value: 1,
   },
   radiusYMultiplier: {
-    name: 'Radius Y Multiplier',
-    description: '',
+    name: 'Radius Noise Y Multiplier',
+    description: 'Multiplies against the Y value in the noise generator for radius',
     value: 1,
   },
   rotationXMultiplier: {
     name: 'Rotation X Multiplier',
-    description: '',
+    description: 'Multiplies against the X value in the noise generator for rotation',
     value: 1,
   },
   rotationYMultiplier: {
     name: 'Rotation Y Multiplier',
-    description: '',
+    description: 'Multiplies against the Y value in the noise generator for rotation',
     value: 1,
   },
 }
