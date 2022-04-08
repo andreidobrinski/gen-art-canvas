ae2128cb272c9b13b411faf038e3fdd73109a720
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-04-07 21:07:53 -0400

    separate initialValues to own file

--- src/App.js
+++ src/App.js
@@ -1,66 +1,14 @@
 import { useReducer } from 'react';
 import styled from 'styled-components';
 import { Sketch } from './Sketch';
-
-const allParams = {
-  gridCount: {
-    name: 'Grid Count',
-    description: 'The number of rows and columns in the grid',
-    value: 40,
-  },
-  filterPercent: {
-    name: 'Filter Percent',
-    description: 'The percent of symbols to randomly filter out (0-100)',
-    value: 20,
-  },
-  colorCount: {
-    name: 'Colour Count',
-    description: 'The number of colours to use from the palette (0-5)',
-    value: 5,
-  },
-  symbol: {
-    name: 'Symbol',
-    description: 'The Unicode symbol used in each cell. Try a |, =, or an emoji',
-    value: '.',
-  },
-  rotationMultiplier: {
-    name: 'Rotation Multiplier',
-    description: 'Total rotation multiplier for each cell',
-    value: 1,
-  },
-  radiusMultiplier: {
-    name: 'Radius Multiplier',
-    description: 'Total radius multiplier for each cell. Think of this as the size.',
-    value: 0.25,
-  },
-  radiusXMultiplier: {
-    name: 'Radius Noise X Multiplier',
-    description: 'Multiplies against the X value in the noise generator for radius',
-    value: 1,
-  },
-  radiusYMultiplier: {
-    name: 'Radius Noise Y Multiplier',
-    description: 'Multiplies against the Y value in the noise generator for radius',
-    value: 1,
-  },
-  rotationXMultiplier: {
-    name: 'Rotation X Multiplier',
-    description: 'Multiplies against the X value in the noise generator for rotation',
-    value: 1,
-  },
-  rotationYMultiplier: {
-    name: 'Rotation Y Multiplier',
-    description: 'Multiplies against the Y value in the noise generator for rotation',
-    value: 1,
-  },
-}
+import { initialValues } from './initialValues';
 
 const reducer = (state, action) => ({
   ...state,
   [action.type]: action.value
 })
 
-const initialState = Object.entries(allParams).reduce((acc, current) => {
+const initialState = Object.entries(initialValues).reduce((acc, current) => {
   return {
     ...acc,
     [current[0]]: current[1].value
@@ -76,7 +24,7 @@ export function App() {
       <Grid>
         {Object.entries(state).map(([key, value]) => (
           <ParamWrap key={key}>
-            <label>{allParams[key].name}</label>
+            <label>{initialValues[key].name}</label>
             <input
               value={value}
               onChange={({ target: { value } }) => dispatch({ type: key, value })}
---
+++ src/initialValues.js
@@ -0,0 +1,52 @@
+export const initialValues = {
+  gridCount: {
+    name: 'Grid Count',
+    description: 'The number of rows and columns in the grid',
+    value: 40,
+  },
+  filterPercent: {
+    name: 'Filter Percent',
+    description: 'The percent of symbols to randomly filter out (0-100)',
+    value: 20,
+  },
+  colorCount: {
+    name: 'Colour Count',
+    description: 'The number of colours to use from the palette (0-5)',
+    value: 5,
+  },
+  symbol: {
+    name: 'Symbol',
+    description: 'The Unicode symbol used in each cell. Try a |, =, or an emoji',
+    value: '.',
+  },
+  rotationMultiplier: {
+    name: 'Rotation Multiplier',
+    description: 'Total rotation multiplier for each cell',
+    value: 1,
+  },
+  radiusMultiplier: {
+    name: 'Radius Multiplier',
+    description: 'Total radius multiplier for each cell. Think of this as the size.',
+    value: 0.25,
+  },
+  radiusXMultiplier: {
+    name: 'Radius Noise X Multiplier',
+    description: 'Multiplies against the X value in the noise generator for radius',
+    value: 1,
+  },
+  radiusYMultiplier: {
+    name: 'Radius Noise Y Multiplier',
+    description: 'Multiplies against the Y value in the noise generator for radius',
+    value: 1,
+  },
+  rotationXMultiplier: {
+    name: 'Rotation X Multiplier',
+    description: 'Multiplies against the X value in the noise generator for rotation',
+    value: 1,
+  },
+  rotationYMultiplier: {
+    name: 'Rotation Y Multiplier',
+    description: 'Multiplies against the Y value in the noise generator for rotation',
+    value: 1,
+  },
+};
